cbe176c1ede5d775ff8c6c7b28f5c7e5576e851f
Vinicius Andrade <fabioviniciusbarbosa@hotmail.com>
2024-07-11 10:38:44 -0300

    Inicio da adição de cartões

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const homeController = require("./controllers/homeController");
 const loginController = require("./controllers/loginController");
 const transacaoController = require("./controllers/transacaoController");
+const cartaoController = require("./controllers/cartaoController");
 
 //middleware
 const { loginRequired } = require("./middlewares/middleware");
@@ -25,5 +26,8 @@ router.post("/transacao/register", loginRequired, transacaoController.register);
 router.get("/transacao/index/:id", loginRequired, transacaoController.edtTransacao);
 router.post("/transacao/edt/:id", loginRequired, transacaoController.editar);
 router.get("/transacao/delete/:id", loginRequired, transacaoController.delete);
+
+//cartao
+router.get("/cartao", loginRequired, cartaoController.index);
 
 module.exports = router;
